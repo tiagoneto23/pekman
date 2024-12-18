ac2942fccbc75dfd3202400247a8dab1ab6e62d3
tiagoneto23 <trabalhospt013@gmail.com>
2024-12-18 10:21:07 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(name='pekman',
       description='pekman Distribution Module',
       author='Vasco Trindade, Tiago Neto, Felipe Candido',
       author_email='felipe.candido@ubi.pt, tiago.neto@ubi.pt, vasco.trindade@ubi.pt',
-      url='https://github.com/ubi-datasciencelabs-students/IACD_Prog_20242025_Grupo8.git'
+      url='https://github.com/tiagoneto23/pekman.git'
       py_modules=['pekman']
      )
