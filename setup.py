396e26b942379f0b8f67742bbc78ca70676c1a82
tiagoneto23 <trabalhospt013@gmail.com>
2024-12-18 10:11:57 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='pekman',
+      version='1.0',
+      description='pekman Distribution Module',
+      author='Vasco Trindade, Tiago Neto, Felipe Candido',
+      author_email='felipe.candido@ubi.pt, tiago.neto@ubi.pt, vasco.trindade@ubi.pt',
+      url='https://github.com/ubi-datasciencelabs-students/IACD_Prog_20242025_Grupo8.git'
+      py_modules=['pekman']
+     )
