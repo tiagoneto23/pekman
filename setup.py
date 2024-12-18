c04138a3ca420b6e9ea9240484477f76df627718
tiagoneto23 <trabalhospt013@gmail.com>
2024-12-18 10:22:15 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
-setup(name='pekman',
-      version='1.0',
-      description='pekman Distribution Module',
-      author='Vasco Trindade, Tiago Neto, Felipe Candido',
-      author_email='felipe.candido@ubi.pt, tiago.neto@ubi.pt, vasco.trindade@ubi.pt',
-      url='https://github.com/tiagoneto23/pekman.git'
-      py_modules=['pekman']
-     )
+
+setup(
+    name='pekman',
+    version='1.0',
+    description='pekman Distribution Module',
+    author='Vasco Trindade, Tiago Neto, Felipe Candido',
+    author_email='felipe.candido@ubi.pt, tiago.neto@ubi.pt, vasco.trindade@ubi.pt',
+    url='https://github.com/tiagoneto23/pekman.git',
+    py_modules=['pekman']
+)
